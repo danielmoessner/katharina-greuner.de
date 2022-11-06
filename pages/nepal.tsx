910e831888ac5d4dbd511766aaa92279f122bdfc
Thies Pooch <thiespooch@icloud.com>
2022-11-06 18:47:53 +0100

    design

--- pages/nepal.tsx
+++ pages/nepal.tsx
@@ -8,13 +8,11 @@ import SectionStart from "components/SectionStart";
 import ContentImageText from "components/ContentImageText";
 import SectionContent from "components/SectionContent";
 import ContentMarkdownImage from "components/ContentMarkdownImage";
-import ContentImageMarkdown from "components/ContentImageMarkdown";
 import ContentMarkdown from "components/ContentMarkdown";
 import Link from "next/link";
 import Heading from "components/Heading";
 import Button from "components/Button";
 import ImageBorder from "components/ImageBorder";
-import Multiline from "components/Multiline";
 import Image from "next/image";
 
 function Page({ pageData }) {
@@ -26,45 +24,65 @@ function Page({ pageData }) {
       <Header header={page.header} />
 
       <SectionStart start={page.start}>
-        <ContentImageText
-          text={page.start.markdown.html}
-          image={page.start.image}
-          alt={page.start.text}
-        />
-        <ContentMarkdownImage
-          html={page.start.markdown2.html}
-          image={page.start.image2}
-          alt={page.start.text}
-        />
+        <div className="grid gap-y-12">
+          <div className="grid items-center gap-x-12 ">
+            <ContentImageText
+              text={page.start.markdown.html}
+              image={page.start.image}
+              alt={page.start.text}
+            />
+          </div>
+
+          <div className="grid items-centergap-x-12 ">
+            <ContentMarkdownImage
+              html={page.start.markdown2.html}
+              image={page.start.image2}
+              alt={page.start.text}
+            />
+          </div>
+        </div>
       </SectionStart>
 
       <SectionFlowerBackground>
-        <Heading>{page.description.title}</Heading>
-        <ContentMarkdown html={page.description.markdown.html} />
+        <div className="grid items-center my-8 gap-x-12 gap-y-8">
+          <div className="grid justify-center ">
+            <Heading>{page.description.title}</Heading>
+          </div>
+          <div className="grid items-center text-left ">
+            <ContentMarkdown html={page.description.markdown.html} />
+          </div>
+        </div>
       </SectionFlowerBackground>
 
       <SectionContent>
-        <Heading>{page.activities.title}</Heading>
-        <div className="grid gap-x-12 gap-y-4 md:grid-cols-2">
+        {/* content left */}
+        <div className="grid grid-cols-2 place-content-center gap-x-12 gap-y-4">
           <div>
-            {/* missing links */}
-            <p>
-              Wenn Du mehr über unser Projekt erfahren möchtest, findest Du
-              Berichte und Jahresrückblicke in meinem Blog.
-            </p>
-            <div>
-              {page.activities.links.map((item) => (
-                <Link key={item.text} href={item.url}>
-                  <a>{item.text}</a>
-                </Link>
-              ))}
+            <Heading>
+              <div className=""> {page.activities.title}</div>
+            </Heading>
+            <div className="justify-center">
+              <p className="my-4 ">
+                Wenn Du mehr über unser Projekt erfahren möchtest, findest Du
+                Berichte und Jahresrückblicke in meinem Blog.
+              </p>
+              <div className="flex flex-col justify-center gap-y-1">
+                {page.activities.links.map((item) => (
+                  <Link key={item.text} href={item.url}>
+                    <a>{item.text}</a>
+                  </Link>
+                ))}
+                <div className="flex self-stretch mt-6 ">
+                  <Button href={page.activities.cta.url}>
+                    {page.activities.cta.text}
+                  </Button>
+                </div>
+              </div>
             </div>
-            {/* button content missing */}
-            <Button href={page.activities.cta.url}>
-              {page.activities.cta.text}
-            </Button>
           </div>
-          <div>
+
+          {/* image right */}
+          <div className="">
             <ImageBorder>
               <Image {...page.activities.image} alt={page.activities.alt} />
             </ImageBorder>
