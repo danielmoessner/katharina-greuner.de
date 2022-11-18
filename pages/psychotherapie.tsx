3d27dc9905370f3bc86ba37c8bc395f4980083ab
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-11-18 11:31:57 +0100

    psychotherapie fertig

--- pages/psychotherapie.tsx
+++ pages/psychotherapie.tsx
@@ -3,14 +3,10 @@ import Seo from "../components/Seo";
 import Header from "../components/Header";
 import { renderContent } from "lib/renderContent";
 import SectionStart from "components/SectionStart";
-import SectionContent from "components/SectionContent";
 import ContentMarkdownImage from "components/ContentMarkdownImage";
 import psychotherapie from "content/page/psychotherapie.json";
 import ContentMarkdown from "components/ContentMarkdown";
-import Heading from "components/Heading";
 import Button from "components/Button";
-import ImageBorder from "components/ImageBorder";
-import Image from "next/image";
 import ContentImageMarkdown from "components/ContentImageMarkdown";
 
 function Page({ pageData }) {
@@ -34,21 +30,19 @@ function Page({ pageData }) {
           <div className="grid items-center text-left ">
             <ContentMarkdown html={page.start.markdown2.html} />
           </div>
+
+          <div className="">
+            <ContentImageMarkdown
+              html={page.procedures.markdown.html}
+              image={page.procedures.image}
+              alt={page.procedures.text}
+            />
+          </div>
+          <div className="flex justify-center mt-6 ">
+            <Button href={page.cta.url}>{page.cta.text}</Button>
+          </div>
         </div>
       </SectionStart>
-
-      <SectionContent>
-        <div className="">
-          <ContentImageMarkdown
-            html={page.procedures.markdown.html}
-            image={page.procedures.image}
-            alt={page.procedures.text}
-          />
-        </div>
-        <div className="flex justify-center mt-6 ">
-          <Button href={page.cta.url}>{page.cta.text}</Button>
-        </div>
-      </SectionContent>
     </Layout>
   );
 }
