51355ad582f1392c3bb5059014aa5601cac51983
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-11-18 11:46:12 +0100

    fix

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,8 +1,3 @@
-// // import { graphql } from 'gatsby';
-// import React from "react";
-// // import PropTypes from 'prop-types';
-// // import { Link, graphql } from 'gatsby';
-// // import { Transition } from '@headlessui/react';
 import Container from "../components/Container";
 import Layout from "../components/Layout";
 
@@ -16,29 +11,4 @@ function Page() {
   );
 }
 
-// Page.defaultProps = {};
-
-// Page.propTypes = {
-//   // eslint-disable-next-line
-//   // data: PropTypes.object.isRequired,
-// };
-
 export default Page;
-
-// // export const query = graphql`
-// //   {
-// //     pagesYaml(slug: { eq: "NONE" }) {
-// //       meta {
-// //         image {
-// //           childImageSharp {
-// //             resize(width: 1200) {
-// //               src
-// //             }
-// //           }
-// //         }
-// //         description
-// //         title
-// //       }
-// //     }
-// //   }
-// // `;
