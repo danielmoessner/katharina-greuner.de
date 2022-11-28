45b9f2c0bdea132a2fb3d92113e0e82fd76f29bc
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-11-28 15:18:41 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,8 +7,8 @@ function Component({ header }) {
         <Image {...header.image} alt={header.title || "Header Bild"} />
       </div>
       {header?.title && (
-        <div className="absolute max-w-sm top-[30%] left-[60%]">
-          <h1 className="text-xl font-bold tracking-tight text-center text-kg-green">
+        <div className="absolute max-w-sm top-[20px] lg:top-[30%] left-[60%]">
+          <h1 className="text-xs font-bold tracking-tight text-center lg:text-xl text-kg-green">
             {header.title}
           </h1>
         </div>
