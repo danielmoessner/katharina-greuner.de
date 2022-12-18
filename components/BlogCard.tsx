ede9ff630c19abc7394b9ac5f7177eb4e1f4c0dd
towezo <96501762+towezo@users.noreply.github.com>
2022-12-18 18:05:06 +0100

    blog data

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { ImageRendered } from "types/shared";
 import Link from "next/link";
 
-interface Seminar {
+interface Blog {
   title: string;
   image: ImageRendered;
   newImage: ImageRendered;
@@ -12,27 +12,27 @@ interface Seminar {
 }
 
 interface Props {
-  seminar: Seminar;
+  blog: Blog;
   button: string;
 }
 
-function SeminarCard({ seminar, button }: Props) {
+function BlogCard({ blog, button }: Props) {
   return (
     <div className="flex flex-col justify-between h-full p-4 border-b-2 bg-kg-green/20 border-b-kg-brown">
       <div>
         <div className="border-b-kg-yellow">
-          <Image {...seminar.newImage} alt={seminar.title} />
+          <Image {...blog.newImage} alt={blog.title} />
         </div>
-        <h2 className="mt-2 text-3xl">{seminar.title}</h2>
+        <h2 className="mt-2 text-3xl">{blog.title}</h2>
         <p className=""> datum </p>
-        <p className="mt-3">{seminar.description}</p>
-        <p className="mt-1 font-bold tracking-wide">{seminar.date}</p>
+        <p className="mt-3">{blog.description}</p>
+        <p className="mt-1 font-bold tracking-wide">{blog.date}</p>
       </div>
       <div className="mt-2">
-        <Link href={`seminar/${seminar.slug}`}>{button}</Link>
+        <Link href={`blog/${blog.slug}`}>{button}</Link>
       </div>
     </div>
   );
 }
 
-export default SeminarCard;
+export default BlogCard;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,23 +2,60 @@ import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 import blog from "../content/page/blog.json";
 import React from "react";
+import pageSource from "content/page/blog.json";
 import Header from "components/Header";
 import Container from "components/Container";
 import { renderContent } from "lib/renderContent";
-import pageSource from "content/page/blog.json";
 import Heading from "components/Heading";
 import ContentMarkdownCtaImage from "components/ContentMarkdownCtaImage";
-import SeminarCard from "components/SeminarCard";
+import BlogCard from "components/BlogCard";
 import { getAllJson } from "lib/getContent";
 import sortBy from "lodash/sortBy";
 
-function Page({ pageData, blog }) {
-  const page = blog;
+function Page({ pageData }) {
+  const page = pageData;
 
   return (
     <Layout>
       <Seo meta={page.meta} />
-      <section>todo</section>
+      <Header header={page.header} />
+      <section className="py-16">
+        <Container layout="sm">
+          <div className="flex justify-center">
+            <Heading element="h1" size="h1">
+              {page.content.title}
+            </Heading>
+          </div>
+          <div className="mt-20">
+            <ContentMarkdownCtaImage
+              html={page.content.markdown.html}
+              cta={page.content.cta}
+              image={page.content.image}
+              alt={page.content.alt}
+            />
+          </div>
+        </Container>
+      </section>
+      <section className="pt-6 pb-20">
+        <Container layout="sm">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            {/* {sortBy(blog, "order").map((blog) => {
+              if (blog === true) {
+                return (
+                  <>
+                    <React.Fragment key={blog.title}>
+                      <BlogCard blog={undefined} button={""} />
+                    </React.Fragment>
+                    <React.Fragment key={blog.title}>
+                      <BlogCard blog={blog} button={page.content.BlogButton} />
+                    </React.Fragment>
+                  </>
+                );
+              }
+            })} */}
+          </div>
+        </Container>
+      </section>
     </Layout>
   );
 }
@@ -27,14 +64,15 @@ export default Page;
 
 export async function getStaticProps() {
   // const footerData = await renderContent(footerSource[locale]);
-  const pageData = await renderContent(blog);
-  const seminars = await renderContent(getAllJson("blog"));
+  // const pageData = await renderContent(blog);
+  const pageData = await renderContent(pageSource);
+  const blog = await renderContent(getAllJson("blog"));
 
   return {
     props: {
       pageData,
       blog,
       // footerData,
-    }, // will be passed to the seminar component as props
+    }, // will be passed to the blog component as props
   };
 }
