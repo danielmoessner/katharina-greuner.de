babc4631a6b40f96028f744cce8ad8e36379bcda
towezo <96501762+towezo@users.noreply.github.com>
2022-12-18 18:36:49 +0100

    vorlage nachbau

--- components/AboutMeCard.tsx
+++ components/AboutMeCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { ImageRendered } from "types/shared";
 import Link from "next/link";
 
-interface Blog {
+interface About {
   title: string;
   image: ImageRendered;
   newImage: ImageRendered;
@@ -12,22 +12,22 @@ interface Blog {
 }
 
 interface Props {
-  blog: Blog;
+  about: About;
   button: string;
 }
 
-function BlogCard({ blog, button }: Props) {
+function AboutCard({ about, button }: Props) {
   return (
     <div className="flex flex-col justify-between h-full p-4 border-b-2 bg-kg-green/20">
       <div>
-        <div className="border-b-kg-yellow">
-          blog image
+        <div className="rounded-full border-b-kg-yellow bg-grey">
+          about image rounded
           {/* <Image {...blog.newImage} alt={blog.title} /> */}
         </div>
-        <h2 className="mt-2 text-3xl"> blog.title</h2>
+        <h2 className="mt-2 text-3xl"> about.title</h2>
         <p className=""> datum </p>
-        <p className="mt-3">blog.description</p>
-        <p className="mt-1 font-bold tracking-wide">blog.date</p>
+        <p className="mt-3">about.description</p>
+        <p className="mt-1 font-bold tracking-wide">about.date</p>
       </div>
       <div className="mt-2">
         {/* <Link href={`blog/${blog.slug}`}>{button}</Link> */}
@@ -36,4 +36,4 @@ function BlogCard({ blog, button }: Props) {
   );
 }
 
-export default BlogCard;
+export default AboutCard;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -11,6 +11,7 @@ import ContentMarkdownCtaImage from "components/ContentMarkdownCtaImage";
 import BlogCard from "components/BlogCard";
 import { getAllJson } from "lib/getContent";
 import sortBy from "lodash/sortBy";
+import AboutCard from "components/AboutMeCard";
 
 function Page({ pageData }) {
   const page = pageData;
@@ -19,42 +20,70 @@ function Page({ pageData }) {
     <Layout>
       <Seo meta={page.meta} />
       <Header header={page.header} />
-      <section className="py-16">
-        <Container layout="sm">
-          <div className="flex justify-center">
-            <Heading element="h1" size="h1">
-              {page.content.title}
-            </Heading>
-          </div>
-          <div className="mt-20">
-            <ContentMarkdownCtaImage
-              html={page.content.markdown.html}
-              cta={page.content.cta}
-              image={page.content.image}
-              alt={page.content.alt}
-            />
-          </div>
-        </Container>
+      <section>
+        <div className="flex justify-center">
+          <Heading element="h1" size="h1">
+            {page.content.title}
+          </Heading>
+        </div>
       </section>
-      <section className="pt-6 pb-20">
-        <Container layout="sm">
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <section className="grid grid-cols-2 divide-x-2">
+        <section className="pt-6 pb-20 divide-y-2">
+          <Container layout="sm">
+            <div className="p-4 border-b-2 ">
+              <BlogCard blog={undefined} button={"test"}></BlogCard>
+            </div>{" "}
+            <div className="p-4 border-b-2">
+              <BlogCard blog={undefined} button={"test"}></BlogCard>
+            </div>
+          </Container>
+        </section>
+
+        <section className="pt-6 pb-20">
+          <Container layout="sm">
+            <div>
+              <AboutCard about={undefined} button={"test"}></AboutCard>
+            </div>
+
+            <div className="mt-4">
+              <h1>Schlagwörter</h1>
+              <ui>
+                <li>text</li>
+                <li>text</li>
+                <li>text</li>
+                <li>text</li>
+                <li>text</li>
+                <li>text</li>
+                <li>text</li>
+              </ui>
+            </div>
+
+            {/* Newsletter Card über CMS */}
+            <div className="mt-4">
+              <h1>Melde dich hier zum Newsletter an</h1>
+              <div>
+                <p>input</p>
+                <p>input</p>
+                <p>CTA</p>
+                <p>text</p>
+              </div>
+            </div>
             {/* {sortBy(blog, "order").map((blog) => {
               if (blog === true) {
                 return (
                   <>
-                    <React.Fragment key={blog.title}>
-                      <BlogCard blog={undefined} button={""} />
-                    </React.Fragment>
-                    <React.Fragment key={blog.title}>
-                      <BlogCard blog={blog} button={page.content.BlogButton} />
-                    </React.Fragment>
+                  <React.Fragment key={blog.title}>
+                  <BlogCard blog={undefined} button={""} />
+                  </React.Fragment>
+                  <React.Fragment key={blog.title}>
+                  <BlogCard blog={blog} button={page.content.BlogButton} />
+                  </React.Fragment>
                   </>
-                );
-              }
-            })} */}
-          </div>
-        </Container>
+                  );
+                }
+              })} */}
+          </Container>
+        </section>
       </section>
     </Layout>
   );
@@ -66,7 +95,7 @@ export async function getStaticProps() {
   // const footerData = await renderContent(footerSource[locale]);
   // const pageData = await renderContent(blog);
   const pageData = await renderContent(pageSource);
-  const blog = await renderContent(getAllJson("blog"));
+  // const blog = await renderContent(getAllJson("blog"));
 
   return {
     props: {
